bad7a231523845f5cb64867b3ae286739a1c2ca9
taete000 <128807237+taete000@users.noreply.github.com>
2023-04-08 21:47:29 +0700

    Update Ui.lua

--- Ui.lua
+++ Ui.lua
@@ -1,7 +1,6 @@
 --[[ Pepsi's UI Library
 Better and updated web-based docs are planned in distant future.
 Library v0.36 [
-CoreGui:FindFirstChild():Destroy()
     CreateWindow: Function (
         (table | nil) Options [
             (string | nil) Name = "Window Name"
